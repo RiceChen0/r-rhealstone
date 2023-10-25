3f0a63e954ecfa222f7fc3c385ba0b446d96c1c6
RiceChen <rice_best_131@163.com>
2023-10-25 14:13:58 +0800

    合并任务创建和任务启动接口

--- adapter/ipc/rt-thread/rst_task.c
+++ adapter/ipc/rt-thread/rst_task.c
@@ -2,17 +2,11 @@
 #include "rst_ipc.h"
 #include "rtthread.h"
 
-rst_task_id rst_task_create(rst_task_func func, void *arg, rst_task_attr *attr)
+rst_status rst_task_create(rst_task_id *task, rst_task_func func,
+                           void *arg, rst_task_attr *attr)
 {
-    rt_thread_t task = NULL;
-
-    task = rt_thread_create(attr->name, func, arg, attr->stack_size, attr->priority, 5);
-    return (rst_task_id)task;
-}
-
-rst_status rst_task_start(rst_task_id task)
-{
-    rt_thread_startup(task);
+    *task = (rst_task_id)rt_thread_create(attr->name, func, arg, attr->stack_size, attr->priority, 5);
+    rt_thread_startup(*task);
     return RST_OK;
 }
 
